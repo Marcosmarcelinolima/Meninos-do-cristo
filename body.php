5d3a9d3f86bc20c6a2f15253dd4fc66dda67bd45
Marcos Marcelino Lima <marcos.lima@useall.inf.br>
2024-10-08 18:11:33 -0300

    ajustes

---
+++ body.php
@@ -0,0 +1,20 @@
+<div class="container_form">
+        <div class="form">
+            <h3>Por favor preencha os seguintes dados</h3>
+            <form action="#">
+                <label for="email_label" id="email_label">E-mail ou usuário</label>
+                <br><br>
+                <div id="input_email">
+                    <input id="Email" type="email" name="Email" placeholder="">
+                </div>
+                <label id="Senha_label" for="Senha">Senha</label>
+                <div id="input_senha">
+
+                    <input id="Senha" type="password">
+                </div>
+                <br>
+                <button id="entrar">Entrar</button>
+            </form>
+
+        </div>
+    </div>
---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<?php
+include 'head.php';
+?>
+
+<body>
+    <?php
+    include 'body.php';
+    ?>
+
+</body>
+<footer>
+
+</footer>
+
+</html>
